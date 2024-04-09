c514666d6a9ec414caad0da00e9003e09f495aea
FabricioEllerSL <fabricioellersl@gmail.com>
2024-04-09 19:53:40 -0300

    feat: cabecalho tela inicial

--- home/views.py
+++ home/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 
 def first_page(request):
-    return render(request, 'home/inicio.html')
+    data_atual = datetime.now().strftime('%d/%m/%Y')
+    return render(request, 'home/inicio.html', {'data_atual': data_atual})
 
 
 def login_page(request):
