59169885a9001fc1e3b06a81e9d1907048af8a8c
FabricioEllerSL <fabricioellersl@gmail.com>
2024-04-09 09:18:37 -0300

    feat: urls tela principal e login

--- clinica_salutar/urls.py
+++ clinica_salutar/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.first_page, name="home"),
+    path('login/', views.login_page, name="login"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def first_page(request):
+    return HttpResponse('Tela Inicial')
+
+
+def login_page(request):
+    return HttpResponse('Tela de Login')
