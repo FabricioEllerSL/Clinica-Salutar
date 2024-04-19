17f5b75b4074dbf4df78e389f266adfe60e6d640
FabricioEllerSL <fabricioellersl@gmail.com>
2024-04-19 11:55:16 -0300

    feat: html exibir pacientes

--- pacientes/views.py
+++ pacientes/views.py
@@ -1,6 +1,21 @@
+from datetime import datetime
 from django.shortcuts import render
+from pacientes.models import Paciente
 
 # Create your views here.
 
+data_atual = datetime.now().strftime('%d/%m/%Y')
+
 def display(request):
-    return render(request, 'pacientes/display.html')
+    
+    pacientes = Paciente.objects.all()
+
+    print(pacientes, 'aqui')
+
+    context_ = {
+        'data_atual': data_atual,
+        'pacientes': pacientes,
+    }
+
+
+    return render(request, 'pacientes/display.html', context_)
