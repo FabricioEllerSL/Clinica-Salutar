ca624d7062102ee5f2c8d05b7f5f186503006bb6
csivdcifba <138387032+csivdcifba@users.noreply.github.com>
2024-07-16 19:54:56 -0300

    refactor: refatoracao tela info paciente nao concluido

--- pacientes/views.py
+++ pacientes/views.py
@@ -56,39 +56,9 @@ def infos(request, cpf):
 
     paciente = Paciente.objects.get(pk=cpf)
 
-    # metodo para calcular idade do paciente
-
-    def calcular_idade(data_nascimento):
-        data_atual = datetime.now()
-        ano_atual = data_atual.year
-        mes_atual = data_atual.month
-        dia_atual = data_atual.day
-
-        ano_nascimento = data_nascimento.year
-        mes_nascimento = data_nascimento.month
-        dia_nascimento = data_nascimento.day
-
-        idade = ano_atual - ano_nascimento
-
-        # Verifica se ainda não fez aniversário no ano atual
-        if (mes_atual, dia_atual) < (mes_nascimento, dia_nascimento):
-            idade -= 1
-
-        return idade
-
-
-    idade_paciente = calcular_idade(paciente.data_nascimento)
-
-    endereco_paciente = ''
-
-    if paciente.rua and paciente.numero and paciente.bairro:
-        endereco_paciente = paciente.rua + ', No.' + paciente.numero + ' - ' + paciente.bairro
-
     context_ = {
         'data_atual': data_atual,
         'paciente': paciente,
-        'idade_paciente': idade_paciente,
-        'endereco_paciente':endereco_paciente,
     }
 
     return render(request, 'pacientes/infos.html', context_)
