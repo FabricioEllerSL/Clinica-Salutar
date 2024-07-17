c96c1536d3bbd5f87f9a58369115043ded370b32
FabricioEllerSL <fabricioellersl@gmail.com>
2024-07-17 17:57:28 -0300

    feat: formatacao campos funcionario

--- funcionario/forms.py
+++ funcionario/forms.py
@@ -13,12 +13,38 @@ class FuncionarioForm(forms.ModelForm):
         super(FuncionarioForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-input'
-
+            if field_name == 'cpf':
+                field.widget.attrs['class'] = 'form-input campo-cpf'
+            elif field_name == 'cep':
+                field.widget.attrs['class'] = 'form-input campo-cep'
+            elif field_name == 'data_nascimento' or field_name == 'data_admissao':
+                field.widget.attrs['class'] = 'form-input campo-data'
+            elif field_name == 'telefone':
+                field.widget.attrs['class'] = 'form-input campo-telefone'
+            elif field_name == 'rg':
+                field.widget.attrs['class'] = 'form-input campo-rg'
+    
     def clean_cpf(self):
         cpf = self.cleaned_data.get('cpf')
+        cpf = cpf.replace('.', '').replace('-', '')
         if not validar_cpf(cpf):
             raise ValidationError('CPF inválido.')
         return cpf
+    
+    def clean_rg(self):
+        rg = self.cleaned_data.get('rg')
+        rg = rg.replace('.', '').replace('-', '')
+        return rg
+    
+    def clean_cep(self):
+        cep = self.cleaned_data.get('cep')
+        cep = cep.replace('-', '')
+        return cep
+    
+    def clean_telefone(self):
+        telefone = self.cleaned_data.get('telefone')
+        telefone = telefone.replace('(', '').replace(')', '').replace('-', '')
+        return telefone
 
 
     class Meta:
