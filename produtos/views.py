1a4013229c2401334ffea54a63f4f072b9b9da2c
FabricioEllerSL <fabricioellersl@gmail.com>
2024-07-29 23:51:16 -0300

    feat: view display produtos

--- produtos/views.py
+++ produtos/views.py
@@ -1,7 +1,25 @@
-from django.http import HttpResponse
+from produtos.models import Produto
+from datetime import datetime
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+
 
 # Create your views here.
 
+data_atual = datetime.now().strftime('%d/%m/%Y')
+
+@login_required(login_url='home:login')
 def display_produtos(request):
-    return HttpResponse("rota correta")
+
+    """ Essa view exibe os pacientes cadastrados """
+    
+    produtos = Produto.objects.all()
+
+    context_ = {
+        'data_atual': data_atual,
+        'produtos': produtos,
+    }
+
+
+    return render(request, 'produtos/display.html', context_)
