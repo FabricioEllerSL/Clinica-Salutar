4dacba40cbf435d9bf782ced9a8c815a1e321381
FabricioEllerSL <fabricioellersl@gmail.com>
2024-07-30 11:19:22 -0300

    feat: display dos medicos

--- medicos/models.py
+++ medicos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Medico(models.Model):
+    nome = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=20, unique=True)
+    rg = models.CharField(max_length=20)
+    numero_classe = models.CharField(max_length=20)
+    profissao = models.CharField(max_length=20)
--- medicos/views.py
+++ medicos/views.py
@@ -1,6 +1,23 @@
+from datetime import datetime
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from medicos.models import Medico
 
 # Create your views here.
 
+data_atual = datetime.now().strftime('%d/%m/%Y')
+
+@login_required(login_url='home:login')
 def display_medicos(request):
-    return render(request, 'medicos/display.html')
+
+    """ Essa view exibe os pacientes cadastrados """
+    
+    medicos = Medico.objects.all()
+
+    context_ = {
+        'data_atual': data_atual,
+        'medicos': medicos,
+    }
+
+
+    return render(request, 'medicos/display.html', context_)
