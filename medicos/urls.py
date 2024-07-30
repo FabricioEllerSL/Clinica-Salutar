d8c9bacdd3c71d2907e5f42f547581941d90a234
FabricioEllerSL <fabricioellersl@gmail.com>
2024-07-30 11:54:07 -0300

    feat: visualizacao do medico

--- medicos/urls.py
+++ medicos/urls.py
@@ -5,6 +5,8 @@ app_name = 'medicos'
 
 urlpatterns = [
     path('', views.display_medicos, name="display_medicos"),
+    path('search/', views.search, name="search"),
+    path('infos/<int:id>', views.infos, name="infos"),
 
 
 ]
--- medicos/views.py
+++ medicos/views.py
@@ -1,7 +1,8 @@
 from datetime import datetime
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from medicos.models import Medico
+from django.contrib import messages
 
 # Create your views here.
 
@@ -10,7 +11,7 @@ data_atual = datetime.now().strftime('%d/%m/%Y')
 @login_required(login_url='home:login')
 def display_medicos(request):
 
-    """ Essa view exibe os pacientes cadastrados """
+    """ Essa view exibe os medicos cadastrados """
     
     medicos = Medico.objects.all()
 
@@ -21,3 +22,37 @@ def display_medicos(request):
 
 
     return render(request, 'medicos/display.html', context_)
+
+@login_required(login_url='home:login')
+def search(request):
+
+    """ Essa view exibe o resultado da pesquisa pelo medico """
+
+    search_value = request.GET.get('q', '').strip()
+    medicos = Medico.objects.filter(nome__icontains=search_value).order_by('nome')
+
+    if not medicos:
+        messages.error(request, 'Médico não encontrado!')
+        return redirect('medicos:display_medicos')
+    
+    context = {
+        'medicos': medicos,
+        'page_title': 'Pesquisa - ',
+        'search_value': search_value,
+    }
+
+    return render(request, 'medicos/display.html', context)
+
+@login_required(login_url='home:login')
+def infos(request, id):
+
+    """ Essa view exibe as informações do medico  """
+
+    medico = Medico.objects.get(pk=id)
+
+    context_ = {
+        'data_atual': data_atual,
+        'medico': medico,
+    }
+
+    return render(request, 'medicos/infos.html', context_)
